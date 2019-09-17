8661d6f846dadf352fc180651093854ab286cfcf
Graduate <m.h.q.1414@hotmail.com>
2019-09-17 14:25:19 +0300

    Adding new files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+
+
+let x  = (inp) => {
+  y = Object.entries(obj);
+  z = y.map( (inpi) => {
+       inpi[0] + inpi[1]  }
+   return z;
+}
+
